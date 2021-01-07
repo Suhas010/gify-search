1d46718657db216c154e585b9a32068c5c5038cc
Suhas More <suhas.more@joshsoftware.com>
2021-01-08 01:02:02 +0530

    added loading comp

---
+++ src/common/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Img } from '.';
+
+const Loading = () => (
+  <div> 
+    <Img src="https://media.giphy.com/media/iCbBAJpfvm17G/giphy.gif" />
+  </div>
+);
+
+export default Loading;
--- src/common/index.js
+++ src/common/index.js
@@ -1,8 +1,10 @@
 import Img from './Img';
+import Loading from './Loading';
 import { Notification, ConfirmBuy } from './Notification';
 
 export {
   Img,
   Notification,
   ConfirmBuy,
+  Loading
 };
